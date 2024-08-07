1233ebe67026dd73ff77d45f1935c322d70556ba
konterfai <konterfai@datenschmutz.space>
2024-08-07 08:24:22 +0200

    add language codes dictionary
    Signed-off-by: konterfai <konterfai@datenschmutz.space>

---
+++ pkg/helpers/dictionaries/languagecodes.go
@@ -0,0 +1,32 @@
+package dictionaries
+
+var LanguageCodes = []string{
+	"en", // English
+	"es", // Spanish
+	"fr", // French
+	"de", // German
+	"zh", // Chinese
+	"ja", // Japanese
+	"ko", // Korean
+	"ru", // Russian
+	"it", // Italian
+	"pt", // Portuguese
+	"ar", // Arabic
+	"nl", // Dutch
+	"sv", // Swedish
+	"pl", // Polish
+	"tr", // Turkish
+	"da", // Danish
+	"fi", // Finnish
+	"he", // Hebrew
+	"hi", // Hindi
+	"no", // Norwegian
+	"el", // Greek
+	"cs", // Czech
+	"hu", // Hungarian
+	"id", // Indonesian
+	"ms", // Malay
+	"ro", // Romanian
+	"th", // Thai
+	"vi", // Vietnamese
+}
